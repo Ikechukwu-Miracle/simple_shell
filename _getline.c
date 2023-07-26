2127f19b3a4ed64599b3fc5f20d06cce623c0ca7
Ikechukwu Miracle Nweke <nwekeiyke46@gmail.com>
2023-07-26 06:27:47 +0000

    fixed _getline

--- _getline.c
+++ _getline.c
@@ -1,7 +1,7 @@
 #include "shell.h"
 
 static ssize_t read_buffer(char *buffer, ssize_t size, FILE *stream);
-static ssize_t append_character(char **lineptr
+static ssize_t append_character(char **lineptr,
 		ssize_t *length, size_t *capacity, char character);
 /**
  * _getline - reads a line from a file stream
